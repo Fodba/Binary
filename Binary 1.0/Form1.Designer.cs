30936113c3910de763d05061795e3e4f37ec5353
fef <ffodba@gmail.com>
2014-03-31 22:16:06 +0200

    Binary v1.0 terminé
    Signed-off-by: fef <ffodba@gmail.com>

--- Binary 1.0/Form1.Designer.cs
+++ Binary 1.0/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.secondeUnite3 = new System.Windows.Forms.Label();
             this.secondeUnite2 = new System.Windows.Forms.Label();
             this.secondeUnite1 = new System.Windows.Forms.Label();
+            this.heureAnalog = new System.Windows.Forms.Label();
             this.groupBoxHeures.SuspendLayout();
             this.groupBoxMinutes.SuspendLayout();
             this.groupBoxSecondes.SuspendLayout();
@@ -60,47 +61,47 @@
             // 
             // HeureDizaine2
             // 
-            this.HeureDizaine2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.HeureDizaine2.Location = new System.Drawing.Point(6, 66);
+            this.HeureDizaine2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.HeureDizaine2.Location = new System.Drawing.Point(6, 85);
             this.HeureDizaine2.Name = "HeureDizaine2";
             this.HeureDizaine2.Size = new System.Drawing.Size(25, 25);
             this.HeureDizaine2.TabIndex = 2;
             // 
             // HeureDizaine1
             // 
-            this.HeureDizaine1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.HeureDizaine1.Location = new System.Drawing.Point(6, 91);
+            this.HeureDizaine1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.HeureDizaine1.Location = new System.Drawing.Point(6, 121);
             this.HeureDizaine1.Name = "HeureDizaine1";
             this.HeureDizaine1.Size = new System.Drawing.Size(25, 25);
             this.HeureDizaine1.TabIndex = 3;
             // 
             // HeureUnite1
             // 
-            this.HeureUnite1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.HeureUnite1.Location = new System.Drawing.Point(37, 91);
+            this.HeureUnite1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.HeureUnite1.Location = new System.Drawing.Point(37, 121);
             this.HeureUnite1.Name = "HeureUnite1";
             this.HeureUnite1.Size = new System.Drawing.Size(25, 25);
             this.HeureUnite1.TabIndex = 7;
             // 
             // HeureUnite2
             // 
-            this.HeureUnite2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.HeureUnite2.Location = new System.Drawing.Point(37, 66);
+            this.HeureUnite2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.HeureUnite2.Location = new System.Drawing.Point(37, 85);
             this.HeureUnite2.Name = "HeureUnite2";
             this.HeureUnite2.Size = new System.Drawing.Size(25, 25);
             this.HeureUnite2.TabIndex = 6;
             // 
             // HeureUnite3
             // 
-            this.HeureUnite3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.HeureUnite3.Location = new System.Drawing.Point(37, 41);
+            this.HeureUnite3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.HeureUnite3.Location = new System.Drawing.Point(37, 51);
             this.HeureUnite3.Name = "HeureUnite3";
             this.HeureUnite3.Size = new System.Drawing.Size(25, 25);
             this.HeureUnite3.TabIndex = 5;
             // 
             // HeureUnite4
             // 
-            this.HeureUnite4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.HeureUnite4.BackColor = System.Drawing.Color.MidnightBlue;
             this.HeureUnite4.Location = new System.Drawing.Point(37, 16);
             this.HeureUnite4.Name = "HeureUnite4";
             this.HeureUnite4.Size = new System.Drawing.Size(25, 25);
@@ -112,60 +113,62 @@
             // 
             // groupBoxHeures
             // 
+            this.groupBoxHeures.BackColor = System.Drawing.Color.MidnightBlue;
             this.groupBoxHeures.Controls.Add(this.HeureDizaine2);
-            this.groupBoxHeures.Controls.Add(this.HeureDizaine1);
             this.groupBoxHeures.Controls.Add(this.HeureUnite4);
             this.groupBoxHeures.Controls.Add(this.HeureUnite3);
+            this.groupBoxHeures.Controls.Add(this.HeureDizaine1);
             this.groupBoxHeures.Controls.Add(this.HeureUnite2);
             this.groupBoxHeures.Controls.Add(this.HeureUnite1);
-            this.groupBoxHeures.Location = new System.Drawing.Point(12, 3);
+            this.groupBoxHeures.Location = new System.Drawing.Point(12, 25);
             this.groupBoxHeures.Name = "groupBoxHeures";
-            this.groupBoxHeures.Size = new System.Drawing.Size(67, 129);
+            this.groupBoxHeures.Size = new System.Drawing.Size(67, 157);
             this.groupBoxHeures.TabIndex = 24;
             this.groupBoxHeures.TabStop = false;
             // 
             // groupBoxMinutes
             // 
-            this.groupBoxMinutes.Controls.Add(this.MinuteDizaine3);
-            this.groupBoxMinutes.Controls.Add(this.MinuteDizaine2);
-            this.groupBoxMinutes.Controls.Add(this.MinuteDizaine1);
+            this.groupBoxMinutes.BackColor = System.Drawing.Color.MidnightBlue;
             this.groupBoxMinutes.Controls.Add(this.MinuteUnite4);
+            this.groupBoxMinutes.Controls.Add(this.MinuteDizaine3);
             this.groupBoxMinutes.Controls.Add(this.MinuteUnite3);
             this.groupBoxMinutes.Controls.Add(this.MinuteUnite2);
+            this.groupBoxMinutes.Controls.Add(this.MinuteDizaine2);
             this.groupBoxMinutes.Controls.Add(this.MinuteUnite1);
-            this.groupBoxMinutes.Location = new System.Drawing.Point(85, 3);
+            this.groupBoxMinutes.Controls.Add(this.MinuteDizaine1);
+            this.groupBoxMinutes.Location = new System.Drawing.Point(85, 25);
             this.groupBoxMinutes.Name = "groupBoxMinutes";
-            this.groupBoxMinutes.Size = new System.Drawing.Size(67, 129);
+            this.groupBoxMinutes.Size = new System.Drawing.Size(67, 157);
             this.groupBoxMinutes.TabIndex = 25;
             this.groupBoxMinutes.TabStop = false;
             // 
             // MinuteDizaine3
             // 
-            this.MinuteDizaine3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.MinuteDizaine3.Location = new System.Drawing.Point(6, 41);
+            this.MinuteDizaine3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.MinuteDizaine3.Location = new System.Drawing.Point(5, 51);
             this.MinuteDizaine3.Name = "MinuteDizaine3";
             this.MinuteDizaine3.Size = new System.Drawing.Size(25, 25);
             this.MinuteDizaine3.TabIndex = 1;
             // 
             // MinuteDizaine2
             // 
-            this.MinuteDizaine2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.MinuteDizaine2.Location = new System.Drawing.Point(6, 66);
+            this.MinuteDizaine2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.MinuteDizaine2.Location = new System.Drawing.Point(5, 85);
             this.MinuteDizaine2.Name = "MinuteDizaine2";
             this.MinuteDizaine2.Size = new System.Drawing.Size(25, 25);
             this.MinuteDizaine2.TabIndex = 2;
             // 
             // MinuteDizaine1
             // 
-            this.MinuteDizaine1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.MinuteDizaine1.Location = new System.Drawing.Point(6, 91);
+            this.MinuteDizaine1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.MinuteDizaine1.Location = new System.Drawing.Point(5, 121);
             this.MinuteDizaine1.Name = "MinuteDizaine1";
             this.MinuteDizaine1.Size = new System.Drawing.Size(25, 25);
             this.MinuteDizaine1.TabIndex = 3;
             // 
             // MinuteUnite4
             // 
-            this.MinuteUnite4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MinuteUnite4.BackColor = System.Drawing.Color.MidnightBlue;
             this.MinuteUnite4.Location = new System.Drawing.Point(37, 16);
             this.MinuteUnite4.Name = "MinuteUnite4";
             this.MinuteUnite4.Size = new System.Drawing.Size(25, 25);
@@ -173,114 +176,129 @@
             // 
             // MinuteUnite3
             // 
-            this.MinuteUnite3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.MinuteUnite3.Location = new System.Drawing.Point(37, 41);
+            this.MinuteUnite3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.MinuteUnite3.Location = new System.Drawing.Point(36, 51);
             this.MinuteUnite3.Name = "MinuteUnite3";
             this.MinuteUnite3.Size = new System.Drawing.Size(25, 25);
             this.MinuteUnite3.TabIndex = 5;
             // 
             // MinuteUnite2
             // 
-            this.MinuteUnite2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.MinuteUnite2.Location = new System.Drawing.Point(37, 66);
+            this.MinuteUnite2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.MinuteUnite2.Location = new System.Drawing.Point(36, 85);
             this.MinuteUnite2.Name = "MinuteUnite2";
             this.MinuteUnite2.Size = new System.Drawing.Size(25, 25);
             this.MinuteUnite2.TabIndex = 6;
             // 
             // MinuteUnite1
             // 
-            this.MinuteUnite1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.MinuteUnite1.Location = new System.Drawing.Point(37, 91);
+            this.MinuteUnite1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.MinuteUnite1.Location = new System.Drawing.Point(36, 121);
             this.MinuteUnite1.Name = "MinuteUnite1";
             this.MinuteUnite1.Size = new System.Drawing.Size(25, 25);
             this.MinuteUnite1.TabIndex = 7;
             // 
             // groupBoxSecondes
             // 
-            this.groupBoxSecondes.Controls.Add(this.secondeDizaine3);
+            this.groupBoxSecondes.BackColor = System.Drawing.Color.MidnightBlue;
             this.groupBoxSecondes.Controls.Add(this.secondeDizaine2);
             this.groupBoxSecondes.Controls.Add(this.secondeDizaine1);
+            this.groupBoxSecondes.Controls.Add(this.secondeDizaine3);
             this.groupBoxSecondes.Controls.Add(this.secondeUnite4);
             this.groupBoxSecondes.Controls.Add(this.secondeUnite3);
             this.groupBoxSecondes.Controls.Add(this.secondeUnite2);
             this.groupBoxSecondes.Controls.Add(this.secondeUnite1);
-            this.groupBoxSecondes.Location = new System.Drawing.Point(158, 3);
+            this.groupBoxSecondes.Location = new System.Drawing.Point(158, 25);
             this.groupBoxSecondes.Name = "groupBoxSecondes";
-            this.groupBoxSecondes.Size = new System.Drawing.Size(67, 129);
+            this.groupBoxSecondes.Size = new System.Drawing.Size(67, 157);
             this.groupBoxSecondes.TabIndex = 25;
             this.groupBoxSecondes.TabStop = false;
             // 
             // secondeDizaine3
             // 
-            this.secondeDizaine3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeDizaine3.Location = new System.Drawing.Point(6, 41);
+            this.secondeDizaine3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeDizaine3.Location = new System.Drawing.Point(5, 51);
             this.secondeDizaine3.Name = "secondeDizaine3";
             this.secondeDizaine3.Size = new System.Drawing.Size(25, 25);
             this.secondeDizaine3.TabIndex = 1;
             // 
             // secondeDizaine2
             // 
-            this.secondeDizaine2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeDizaine2.Location = new System.Drawing.Point(6, 66);
+            this.secondeDizaine2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeDizaine2.Location = new System.Drawing.Point(5, 85);
             this.secondeDizaine2.Name = "secondeDizaine2";
             this.secondeDizaine2.Size = new System.Drawing.Size(25, 25);
             this.secondeDizaine2.TabIndex = 2;
             // 
             // secondeDizaine1
             // 
-            this.secondeDizaine1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeDizaine1.Location = new System.Drawing.Point(6, 91);
+            this.secondeDizaine1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeDizaine1.Location = new System.Drawing.Point(5, 121);
             this.secondeDizaine1.Name = "secondeDizaine1";
             this.secondeDizaine1.Size = new System.Drawing.Size(25, 25);
             this.secondeDizaine1.TabIndex = 3;
             // 
             // secondeUnite4
             // 
-            this.secondeUnite4.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeUnite4.Location = new System.Drawing.Point(37, 16);
+            this.secondeUnite4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeUnite4.Location = new System.Drawing.Point(36, 16);
             this.secondeUnite4.Name = "secondeUnite4";
             this.secondeUnite4.Size = new System.Drawing.Size(25, 25);
             this.secondeUnite4.TabIndex = 4;
             // 
             // secondeUnite3
             // 
-            this.secondeUnite3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeUnite3.Location = new System.Drawing.Point(37, 41);
+            this.secondeUnite3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeUnite3.Location = new System.Drawing.Point(36, 51);
             this.secondeUnite3.Name = "secondeUnite3";
             this.secondeUnite3.Size = new System.Drawing.Size(25, 25);
             this.secondeUnite3.TabIndex = 5;
             // 
             // secondeUnite2
             // 
-            this.secondeUnite2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeUnite2.Location = new System.Drawing.Point(37, 66);
+            this.secondeUnite2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeUnite2.Location = new System.Drawing.Point(36, 85);
             this.secondeUnite2.Name = "secondeUnite2";
             this.secondeUnite2.Size = new System.Drawing.Size(25, 25);
             this.secondeUnite2.TabIndex = 6;
             // 
             // secondeUnite1
             // 
-            this.secondeUnite1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.secondeUnite1.Location = new System.Drawing.Point(37, 91);
+            this.secondeUnite1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.secondeUnite1.Location = new System.Drawing.Point(36, 121);
             this.secondeUnite1.Name = "secondeUnite1";
             this.secondeUnite1.Size = new System.Drawing.Size(25, 25);
             this.secondeUnite1.TabIndex = 7;
+            // 
+            // heureAnalog
+            // 
+            this.heureAnalog.AutoSize = true;
+            this.heureAnalog.BackColor = System.Drawing.Color.MidnightBlue;
+            this.heureAnalog.ForeColor = System.Drawing.Color.White;
+            this.heureAnalog.Location = new System.Drawing.Point(12, 9);
+            this.heureAnalog.Name = "heureAnalog";
+            this.heureAnalog.Size = new System.Drawing.Size(0, 13);
+            this.heureAnalog.TabIndex = 26;
+            this.heureAnalog.Visible = false;
             // 
             // Binary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(233, 140);
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(233, 189);
+            this.Controls.Add(this.heureAnalog);
             this.Controls.Add(this.groupBoxSecondes);
             this.Controls.Add(this.groupBoxMinutes);
             this.Controls.Add(this.groupBoxHeures);
             this.Name = "Binary";
             this.Text = "Binary";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Binary_KeyUp);
             this.groupBoxHeures.ResumeLayout(false);
             this.groupBoxMinutes.ResumeLayout(false);
             this.groupBoxSecondes.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -310,6 +328,7 @@
         private System.Windows.Forms.Label secondeUnite3;
         private System.Windows.Forms.Label secondeUnite2;
         private System.Windows.Forms.Label secondeUnite1;
+        private System.Windows.Forms.Label heureAnalog;
     }
 }
 
